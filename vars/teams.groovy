806a2aa0ddbde0529ceff2a2a7d588fc3ecd7b50
Jawed Salim <jawed.salim@apyl.com>
2022-03-25 10:56:53 +0530

    Update teams.groovy

--- vars/teams.groovy
+++ vars/teams.groovy
@@ -2,7 +2,7 @@ def send(URL_WEBHOOK) {
    
    echo "${URL_WEBHOOK}"
    
-   def resp = """curl --location --request POST '$URL_WEBHOOK' --header 'Content-Type: application/json' --data-raw '{ "text" : "hello" }'""".execute().text
+   def resp = """/usr/bin/curl --location --request POST '$URL_WEBHOOK' --header 'Content-Type: application/json' --data-raw '{ "text" : "hello" }'""".execute().text
    
-   echo "${resp}"
+   echo ">> ${resp}"
 }

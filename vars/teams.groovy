4a216753ab3eb514f7cbf3c051c88b7477bc52a6
Jawed Salim <jawed.salim@apyl.com>
2022-03-31 16:39:34 +0530

    Update teams.groovy

--- vars/teams.groovy
+++ vars/teams.groovy
@@ -2,7 +2,23 @@ def send(URL_WEBHOOK) {
    
    echo "${URL_WEBHOOK}"
    
-   sh """/usr/bin/curl --location --request POST '$URL_WEBHOOK' --header 'Content-Type: application/json' --data-raw '{ "text" : "hello" }'"""
+   def conn = new URL("${URL_WEBHOOK}").openConnection()
+   conn.requestMethod = 'POST'
    
-   echo ">> ${resp}"
+   def msg = '{ "text" : "hello groovy" }'
+   conn.setDoOutput(true)
+   conn.setRequestProperty("Content-Type", "application/json")
+   conn.getOutputStream().write(msg.getBytes("UTF-8"))
+
+   // Send request
+   resp = conn.getResponseCode()
+   println("Response Code : ${resp}")
+
+   if (resp.equals(200)) {
+      println("Message has been sent successfully")
+   } else {
+      println("Error: Message not sent");
+      println(conn.getInputStream().getText())
+   }
+   
 }
